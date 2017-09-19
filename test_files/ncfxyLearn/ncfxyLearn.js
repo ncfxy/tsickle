3a742112fd31e93ca7e46de7073ed3c5ce1cae00
Fan Xiangyu <fan_x@worksap.co.jp>
2017-09-19 21:03:23 +0800

    update ncfxyLearn test cases

--- test_files/ncfxyLearn/ncfxyLearn.js
+++ test_files/ncfxyLearn/ncfxyLearn.js
@@ -1,8 +1,45 @@
-unexported;
-unexported.a;
-unexported.a.b;
+goog.provide('wap.core.ui.contact.UserSelectInputUserCountDialogContent');
+goog.require('goog.ui.Component');
+goog.require('wap.core.app.View');
+/**
+ * @param {!Object} opt_options
+ * @param {(undefined|!goog.dom.DomHelper)=} opt_domHelper
+ */
+wap.core.ui.contact.UserSelectInputUserCountDialogContent = function (opt_options, opt_domHelper) {
+    goog.base(this, opt_domHelper);
+    this.option_ = opt_options || {};
+};
+/**
+ * @override
+ *
+ * @param {!Element} $element
+ * @return {void}
+ */
+wap.core.ui.contact.UserSelectInputUserCountDialogContent.decorateInternal($element, Element);
 {
-    export class Unexported {
-    }
+    goog.base(this, "decorateInternal", $element);
+    wap.core.app.View.getInstance().decorateChildren(this);
 }
-let /** @type {?} */ x;
+/**
+ * @override
+ * @return {void}
+ */
+wap.core.ui.contact.UserSelectInputUserCountDialogContent.enterDocument();
+{
+    goog.base(this, "enterDocument");
+}
+/**
+ * @return {void}
+ */
+wap.core.ui.contact.UserSelectInputUserCountDialogContent.exitDocument();
+{
+    goog.base(this, "exitDocument");
+}
+/**
+ * @return {void}
+ */
+wap.core.ui.contact.UserSelectInputUserCountDialogContent.disposeInternal();
+{
+    goog.base(this, "disposeInternal");
+}
+wap.core.app.View.registerComponent('wap.core.ui.contact.UserSelectInputUserCountDialogContent', wap.core.ui.contact.UserSelectInputUserCountDialogContent);
--- test_files/ncfxyLearn/ncfxyLearn.ts
+++ test_files/ncfxyLearn/ncfxyLearn.ts
@@ -1,6 +1,71 @@
+declare module goog{
+  function provide(name: string): void;
+  function require(name: string): string;
+  function base(me: Object, opt_methodName?: any, ...var_args: any[]): any;
+}
+declare module goog.dom{
+  class DomHelper{
 
-module unexported.a.b {
-  export class Unexported {}
+  }
+}
+declare module goog.ui{
+  class Component{
+    constructor(opt_domHelper?: goog.dom.DomHelper);
+    decorateInternal($element: Element);
+    enterDocument();
+    exitDocument();
+    disposeInternal();
+  }
 }
 
-let x: unexported.a.b.Unexported;
+declare module wap.core.app{
+  class View{
+    static getInstance();
+    static registerComponent(name: string, vo: any);
+  }
+}
+
+
+goog.provide('wap.core.ui.contact.UserSelectInputUserCountDialogContent');
+
+goog.require('goog.ui.Component');
+goog.require('wap.core.app.View');
+
+namespace wap.core.ui.contact{
+  export class UserSelectInputUserCountDialogContent extends goog.ui.Component{
+    option_: Object;
+
+    constructor(opt_options: Object, opt_domHelper?:goog.dom.DomHelper){
+      super(opt_domHelper);
+      this.option_ = opt_options || {};
+    }
+
+    /**
+     * @override
+     * @param {Element} $element 
+     * 
+     */
+    decorateInternal($element: Element){
+      super.decorateInternal($element);
+      wap.core.app.View.getInstance().decorateChildren(this);
+    }
+
+    /**
+     * @override
+     */
+    enterDocument(){
+      super.enterDocument();
+    }
+
+    exitDocument(){
+      super.exitDocument();
+    }
+
+    disposeInternal(){
+      super.disposeInternal();
+    }
+  }
+}
+
+wap.core.app.View.registerComponent('wap.core.ui.contact.UserSelectInputUserCountDialogContent',
+  wap.core.ui.contact.UserSelectInputUserCountDialogContent);
